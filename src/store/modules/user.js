20b75cff99147345126ddfaa35c462f1a0a7c1e7
mhh <1147905003@qq.com>
2022-10-19 18:09:47 +0800

    feat:响应优化处理完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,19 @@
+import { login } from '@/api/user'
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    token: null
+  },
+  mutations: {
+    setToken(state, payload) {
+      state.token = payload
+    }
+  },
+  actions: {
+    async loginAction({ commit }, data) {
+      const res = await login(data)
+      console.log(res)
+      commit('setToken', res)
+    }
+  }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,34 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 const service = axios.create({
   baseURL: '/api',
   timeout: 5000
 })
+// 添加请求拦截器
+service.interceptors.request.use(function(config) {
+  // 在发送请求之前做些什么
+  return config
+}, function(error) {
+  // 对请求错误做些什么
+  console.log('error', error)
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+service.interceptors.response.use(response => {
+  // axios默认加了一层data
+  const { success, message, data } = response.data
+  //   要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // 业务已经错误了 还能进then ? 不能 ！ 应该进catch
+    Message.error(message) // 提示错误消息
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message) // 提示错误信息
+  return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
+})
+
 export default service
